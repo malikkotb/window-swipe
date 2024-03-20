1356def541d8cfa00f0f1d9cd7b0d3dee1d983a1
Malik Kotb <malikkotb@icloud.com>
2024-03-20 19:38:58 +0100

    window swipe

--- app/page.js
+++ app/page.js
@@ -1,23 +1,44 @@
+"use client";
 import { gsap } from "gsap";
+import { useRef, useEffect } from "react";
 
 export default function Home() {
-  
   // most basic way to animate elements with gsap is using methods: to, from, fromTo
-
-  // from() and to() do the same thing but opposite
+  // from() and to() do the same thing, but opposite
+  // to() takes in 2 parameters: the class (aka selector) or a ref (useRef hook), and an object where we can animate CSS (and specific gsap) properties
 
   // we want to aniamte the progress bar (which starts at width: 0%,) TO a 100 percent -> to() method
 
-  // to() takes in 2 parameters: the class (aka selector), and an object where we can animate CSS (and specific gsap) properties
+  const progressRef = useRef(null);
+  const headingRef = useRef(null);
+  useEffect(() => {
+    // gsap.to(".progress", {
+    gsap.to(progressRef.current, {
+      width: "100%",
+      duration: 2,
+      ease: "power4.inOut",
+      // other easing functions (including visualizer): gogle: gsap Eases
+    });
 
-  gsap.to(".progress", {
-    width: "100px",
-  })
+    gsap.from(headingRef.current, {
+      y: "-200", // yPercent for percent value
+      duration: 2,
+      ease: "elastic.out(1, 0.3)"
+    })
+   
+    gsap.to(progressRef.current, {
+      height: "100%",
+      delay: 2,
+      top: 0,
+      backgroundColor: "white"
+    })
+
+  });
 
   return (
     <main>
-      <h1 className=" text-[30rem] opacity-[10%] text-center mt-[18%]">BORING</h1>
-      <div className="progress bg-[#A1BCFF] h-[3px] w-0 absolute top-[50%]"></div>
+      <h1 ref={headingRef} className="text-[20rem] opacity-[10%] text-center mt-[18%]">BORING</h1>
+      <div ref={progressRef} className="progress bg-[#A1BCFF] h-[3px] w-0 absolute top-[50%]"></div>
     </main>
   );
 }
