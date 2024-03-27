5aac0db58b2c625ccfdddaa5457d1d25ae974e5d
Malik Kotb <malikkotb@icloud.com>
2024-03-27 23:57:42 +0100

    gsap react

--- app/stagger-tween/page.js
+++ app/stagger-tween/page.js
@@ -1,32 +1,57 @@
 "use client";
-import { gsap } from "gsap";
-import { useRef, useEffect } from "react";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
 
 export default function page() {
+  const sectionRef = useRef();
 
-  // gsap.to(['h1', 'span'], {}) // how multiple elements can be selected at once
-  useEffect(() => {
-    // set method (basically same as from() or to(), except it is with a duration of 0)
-    gsap.set("img", {
-      yPercent: -100, // initially hide all images above (this is possible as they are inside a mask)
-    });
+  useGSAP(
+    () => {
+      gsap.set("img", {
+        yPercent: -100, // initially hide all images above (this is possible as they are inside a mask)
+      });
 
-    gsap.set("p", {
-      opacity: 0,
-    });
+      gsap.set("p", {
+        opacity: 0,
+      });
 
-    // next, we want to slide the images into place, in a staggered mannor
-    gsap.to("img", {
+      // next, we want to slide the images into place, in a staggered mannor
+      gsap.to("img", {
         yPercent: 0,
         scale: 1.3,
-        delay: 0.5,
-        // stagger: 0.2,
-    });
-  });
+        stagger: 0.2,
+        ease: "power4.out",
+      });
+    },
+    { scope: sectionRef }
+  ); // <-- scope is for selector text (optional)
+
+  // gsap.to(['h1', 'span'], {}) // how multiple elements can be selected at once
+  //   useEffect(() => {
+  //     // set method (basically same as from() or to(), except it is with a duration of 0)
+  //     gsap.set("img", {
+  //       yPercent: -100, // initially hide all images above (this is possible as they are inside a mask)
+  //     });
+
+  //     gsap.set("p", {
+  //       opacity: 0,
+  //     });
+
+  //     // next, we want to slide the images into place, in a staggered mannor
+  //     gsap.to("img", {
+  //       yPercent: 0,
+  //       scale: 1.3,
+  //       stagger: 0.1,
+  //       ease: "power4.out",
+  //     });
+  //   });
 
   return (
     <div className="m-0 h-screen grid place-content-center">
-      <section className="flex justify-between gap-4">
+      <section ref={sectionRef} className="flex justify-between gap-4">
         <div className="mask overflow-hidden relative">
           <img className=" w-full h-[80vh]" src="1.jpg" alt="img1" />
           <p className="stagger-tween-p">living</p>
