e8b441e41f025b3ea68ee856098a40cc283df2ca
Malik Kotb <malikkotb@icloud.com>
2024-03-30 12:17:42 +0100

    timeline loader

--- app/timeline/page.js
+++ app/timeline/page.js
@@ -11,13 +11,14 @@ export default function page() {
 
   return (
     <div className="h-screen bg-white m-0">
-      <div className="fade-container">
-        <div className="fade1"></div>
-        <div className="fade2"></div>
-        <div className="fade3"></div>
+      <div className="fade-container h-screen grid grid-rows-3 grid-cols-1 ">
+        {/* add to indiviudal fade divs: // transform: scaleY(0); */}
+        <div className="fade1 transform origin-top bg-[#C9D6EA]"></div>
+        <div className="fade2 transform origin-top bg-[#5E9EFF]"></div>
+        <div className="fade3 transform origin-top bg-[#2C80FF]"></div>
       </div>
-      <div className="mask">
-        <h1 className="text 5xl">Oceana.</h1>
+      <div className="mask overflow-hidden absolute top-[50%] left-[50%] translate-x-1/2 -translate-y-1/2 z-1">
+        <h1 className=" text-9xl bebas text-white m-0 font-black z-1">Oceana.</h1>
       </div>
     </div>
   );
